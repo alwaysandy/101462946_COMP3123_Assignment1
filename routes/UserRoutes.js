fa07615ef6c54578d17ea2ae7b78366bd0ae687f
alwaysandy <andrewdaurio@gmail.com>
2025-10-07 21:41:35 -0400

    Complete adding new user route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,16 +1,49 @@
 const userModel = require("../models/UsersModel");
 const express = require('express');
+const {body, validationResult} = require('express-validator');
 const userRoutes = express.Router();
 
-userRoutes.post('signup', (req, res) => {
-    if(!req.body.content) {
+userRoutes.post('/signup', [
+    body('username')
+        .trim()
+        .notEmpty()
+        .withMessage('Username is required'),
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email is required')
+        .bail()
+        .isEmail()
+        .withMessage('Invalid email format'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('Password is required')
+        .bail()
+        .isLength({min:6})
+        .withMessage('Password must be minimum 6 characters')
+], async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
         return res.status(400).send({
-            message: "Request body can not be empty"
+            status: false,
+            message: result.array()
         });
     }
 
-    return res.sendStatus(404);
-    // TODO - Handle Signup
+    const user = new userModel(req.body);
+    try {
+        const newUser = await user.save();
+        return res.status(201).send({
+            message: "User created successfully",
+            user_id: newUser._id
+        });
+    } catch (err) {
+        return res.status(500).send({
+            status: false,
+            message: err
+        });
+    }
 });
 
 userRoutes.post('login', (req, res) => {
