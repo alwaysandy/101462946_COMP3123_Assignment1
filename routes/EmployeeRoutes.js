4c019e5136686d29c4d3f430b738370dc5b953cf
alwaysandy <andrewdaurio@gmail.com>
2025-10-08 13:19:45 -0400

    Split validation into reusable components

--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -3,6 +3,29 @@ const express = require('express');
 const {body, query, param, validationResult} = require('express-validator');
 const employeeRoutes = express.Router();
 
+const firstNameValidation = () => body('first_name')
+    .trim().notEmpty().withMessage('Must include first name');
+const lastNameValidation = () => body('last_name')
+    .trim().notEmpty().withMessage('Must include last name');
+const emailValidation = () => body('email')
+    .trim().notEmpty().withMessage('Must include email').bail()
+    .isEmail().withMessage('Invalid email address');
+const positionValidation = () => body('position')
+    .trim().notEmpty().withMessage('Must include position');
+const salaryValidation = () => body('salary')
+    .notEmpty().withMessage('Must include salary').bail()
+    .isNumeric().withMessage('Salary must be a number').bail()
+    .isInt({min: 0}).withMessage('Salary must be positive');
+const dateOfJoiningValidation = () => body('date_of_joining')
+    .notEmpty().withMessage('Must include date of joining').bail()
+    .isISO8601().toDate().withMessage('Date of joining must be a date');
+const departmentValidation = () => body('department')
+    .trim().notEmpty().withMessage('Must include department');
+
+const mongoIdParamValidation = () => param('employeeId')
+    .isMongoId().withMessage("Parameter must be valid MongoDB ID");
+
+
 employeeRoutes.get('/employees', async (req, res) => {
     try {
         const employees = await employeeModel.find({});
@@ -15,30 +38,19 @@ employeeRoutes.get('/employees', async (req, res) => {
 });
 
 employeeRoutes.post('/employees', [
-    body('first_name')
-        .trim().notEmpty().withMessage('Must include first name'),
-    body('last_name')
-        .trim().notEmpty().withMessage('Must include last name'),
-    body('email')
-        .trim().notEmpty().withMessage('Must include email').bail()
-        .isEmail().withMessage('Invalid email address'),
-    body('position')
-        .trim().notEmpty().withMessage('Must include position'),
-    body('salary')
-        .notEmpty().withMessage('Must include salary').bail()
-        .isNumeric().withMessage('Salary must be a number').bail()
-        .isInt({min: 0}).withMessage('Salary must be positive'),
-    body('date_of_joining')
-        .notEmpty().withMessage('Must include date of joining').bail()
-        .isISO8601().toDate().withMessage('Date of joining must be a date'),
-    body('department')
-        .trim().notEmpty().withMessage('Must include department')
+    firstNameValidation(),
+    lastNameValidation(),
+    emailValidation(),
+    positionValidation(),
+    salaryValidation(),
+    dateOfJoiningValidation(),
+    departmentValidation()
 ], async (req, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
         return res.status(400).send({
             status: false,
-            errors: result.array().map(error => error.msg)
+            message: result.array().map(error => error.msg)
         });
     }
 
@@ -56,11 +68,7 @@ employeeRoutes.post('/employees', [
     }
 });
 
-employeeRoutes.get('/employees/:employeeId', 
-    param('employeeId')
-        .isMongoId()
-        .withMessage("Parameter must be valid MongoDB ID"), 
-    async (req, res) => {
+employeeRoutes.get('/employees/:employeeId', mongoIdParamValidation(), async (req, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
         return res.status(400).send({
