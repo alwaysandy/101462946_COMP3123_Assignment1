5e11378b8e83df9c0e33454d59e70e7411f79d39
alwaysandy <andrewdaurio@gmail.com>
2025-10-07 22:09:35 -0400

    Hash passwords before saving, allow comparing hashed passwords

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -14,5 +15,17 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 }, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10);
+    }
+    next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('users', userSchema)
